736faa1b2cef396ffefcfced3d8f2a4df245d882
bot0008 <113306232+bot0008@users.noreply.github.com>
2023-02-11 13:10:23 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import Navbar from './components/Navbar';
+import Routing from './components/Routing';
+import Footer from './components/Footer';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './App.css'
 
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-   header
+    <>
+      <Navbar/>
+      <Routing />
+      <Footer />
+    </>
   );
 }
-
-export default App;
